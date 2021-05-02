9d8572627e0efd4bcd6a671167eacc58eb421207
gracklewontsing <jera_3676@hotmail.com>
2021-05-02 13:21:45 -0600

    added nav elements

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,36 @@
 <template>
-  
+  <div>
+    <b-navbar class="mx-5 my-3">
+      <b-navbar-brand href="#">[[LOGO]]</b-navbar-brand>
+      <div class="ml-auto border-bottom my-3 py-2">
+        <b-navbar-nav class="mx-3">                
+          <b-nav-item class="mx-3">Inicio</b-nav-item>        
+          <b-nav-item class="mx-3">Sobre Nosotros</b-nav-item>        
+          <b-nav-item-dropdown text="Productos" class="mx-3">
+            <b-dropdown-item></b-dropdown-item>
+            <b-dropdown-item></b-dropdown-item>
+            <b-dropdown-item></b-dropdown-item>
+          </b-nav-item-dropdown>      
+          <b-nav-item class="mx-3">Contacto</b-nav-item>        
+          <b-nav-item class="mx-3">Noticias</b-nav-item>        
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  components:{
+    
+  }
 }
 </script>
 
 <style>
-
+.nav-link {  
+  /*not accurate, will do, also avoid important*/
+  color: mediumturquoise !important
+}
 </style>
